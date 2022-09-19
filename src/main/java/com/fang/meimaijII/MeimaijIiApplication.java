894e2babd724977145fb41ce8c922fb40b6e0fb0
echo <echo84062505@gmail.com>
2022-09-19 11:28:05 +0800

    init

---
+++ src/main/java/com/fang/meimaijII/MeimaijIiApplication.java
@@ -0,0 +1,13 @@
+package com.fang.meimaijII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeimaijIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeimaijIiApplication.class, args);
+	}
+
+}
